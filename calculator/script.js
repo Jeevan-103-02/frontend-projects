30e770cc6ed6d194f7b7a3e993cf17ec91c55b11
Jeevan-103-02 <jeevan1736jeevan@gmail.com>
2026-04-06 16:20:09 +0530

    Update script.js

--- calculator/script.js
+++ calculator/script.js
@@ -1,1 +1,32 @@
+let display = document.getElementById("display");
+let isResultDisplayed = false;
 
+function appendValue(value) {
+    if (isResultDisplayed) {
+        display.value = "";
+        isResultDisplayed = false;
+    }
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        let expression = display.value;
+
+        if (expression === "") return;
+
+        let result = Function("return " + expression)();
+        display.value = result;
+        isResultDisplayed = true;
+    } catch (error) {
+        display.value = "Error";
+    }
+}
